e574623dd1d7a49c08239031127cfb78fa343bc3
Senyu Li <senyu@cs.ualberta.ca>
2021-09-05 10:50:24 -0600

    first commit

---
+++ lab1.py
@@ -0,0 +1,18 @@
+import json
+import sys
+import csv
+import re
+import time
+import ast
+import math
+import requests 
+
+
+
+def main():
+    
+    print(requests.__version__)
+
+
+if __name__ == '__main__':
+    main()
